2b31bb9006f7c40cbe5c76f7e6365264839b6be4
AlexandreFranco3000 <37425782+AlexandreFranco3000@users.noreply.github.com>
2026-02-27 19:18:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'v1-estoque';
+const CACHE_NAME = 'estoque-v2';
 const ASSETS = [
+  './',
   'index.html',
   'manifest.json',
   'https://cdn.tailwindcss.com',
@@ -8,9 +9,13 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
